4315d56372d97d08e1b7b4bbbff79ea5ca9a9bdf
andi hakim arif <andi.hakim.arif@gmail.com>
2019-07-23 15:09:41 +0700

    lesson 7: showing individual envent / content

--- gatsby-theme-events/src/templates/event.js
+++ gatsby-theme-events/src/templates/event.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Event from "../components/event";
 
-const EventTemplate = () => <p>Todo x </p>;
+// eq = equal
+export const query = graphql`
+  query($eventID: String!) {
+    event(id: { eq: $eventID }) {
+      name
+      url
+      startDate(formatString: "MMMM D, YYYY")
+      endDate(formatString: "MMMM D, YYYY")
+      location
+      slug
+    }
+  }
+`;
+
+// data injected from graphql query
+const EventTemplate = ({ data: { event } }) => (
+  <Layout>
+    <Event {...event} />
+  </Layout>
+);
 
 export default EventTemplate;
