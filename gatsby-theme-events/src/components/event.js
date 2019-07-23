086c3e4e3fb42aa3cd5ed4e3b97838be8fe2d4b2
andi hakim arif <andi.hakim.arif@gmail.com>
2019-07-23 16:11:25 +0700

    lesson 8: styling date format

--- gatsby-theme-events/src/components/event.js
+++ gatsby-theme-events/src/components/event.js
@@ -1,12 +1,47 @@
 import React from "react";
 
+const getDate = (date, { day = true, month = true, year = true } = {}) =>
+  date.toLocaleDateString("en-US", {
+    day: day ? "numeric" : undefined,
+    month: month ? "long" : undefined,
+    year: year ? "numeric" : undefined
+  });
+
+const EventDate = ({ startDate, endDate }) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const isOneDay = start.toDateString() === end.toDateString();
+  console.log(isOneDay);
+  return (
+    <React.Fragment>
+      <time dateTime={start.toISOString()}>
+        {getDate(start, {
+          year: isOneDay || start.getFullYear() !== end.getFullYear()
+        })}
+      </time>
+      {!isOneDay && (
+        <React.Fragment>
+          -
+          <time dateTime={end.toISOString()}>
+            {getDate(end, { month: start.getMonth() !== end.getMonth() })}
+          </time>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
+
 const Event = ({ name, location, startDate, endDate, url }) => (
   <div>
     <h1>
       {name} ({location})
     </h1>
     <p>
-      {startDate}-{endDate}
+      {startDate} - {endDate}
+    </p>
+    <p>
+      <EventDate startDate={startDate} endDate={endDate} />
     </p>
     <p>
       Website: <a href={url}>{url}</a>
